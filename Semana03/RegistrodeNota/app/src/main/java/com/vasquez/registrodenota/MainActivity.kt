0eb15d4ee4c99260ff0eb5286c76ec573e95152a
Angie-67 <angievasquezgg@gmail.com>
2026-09-03 22:57:33 -0500

    Crear el proyecto Registro de Notas y las variables

---
+++ Semana03/RegistrodeNota/app/src/main/java/com/vasquez/registrodenota/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.vasquez.registrodenota
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.vasquez.registrodenota.ui.theme.RegistrodeNotaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RegistrodeNotaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PantallaPrincipal(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PantallaPrincipal(modifier: Modifier){
+    var notFund by remember { mutableFloatStateOf(0f) }
+    var notPOO by remember { mutableFloatStateOf(0f) }
+    var notProg by remember { mutableFloatStateOf(0f) }
+    var notBD by remember { mutableFloatStateOf(0f) }
+
+    var redondear by remember { mutableStateOf(false) }
+    var confirmado by remember { mutableStateOf(false) }
+    var promedioPonderado by remember { mutableDoubleStateOf(0.0) }
+    var promedioFinal by remember { mutableDoubleStateOf(0.0) }
+    var chipColor by remember { mutableStateOf(Color.Gray) }
+    var mostrar by remember { mutableStateOf(false) }
+}
