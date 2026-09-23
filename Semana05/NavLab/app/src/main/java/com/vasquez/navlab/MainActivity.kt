057d7ffe69073af843bd8fb77ac3a54a3ed749c8
Angie-67 <angievasquezgg@gmail.com>
2026-09-23 07:17:05 -0500

    fix: configurar MainActivity.kt, Screen.kt Y XML para iniciar la aplicacion desde AppNavigation.kt

--- Semana05/NavLab/app/src/main/java/com/vasquez/navlab/MainActivity.kt
+++ Semana05/NavLab/app/src/main/java/com/vasquez/navlab/MainActivity.kt
@@ -13,35 +13,4 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.vasquez.navlab.ui.theme.NavLabTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            NavLabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavLabTheme {
-        Greeting("Android")
-    }
-}
--- Semana05/NavLab/app/src/main/java/com/vasquez/navlab/navigation/Screen.kt
+++ Semana05/NavLab/app/src/main/java/com/vasquez/navlab/navigation/Screen.kt
@@ -4,7 +4,7 @@ sealed class Screen(val route: String) {
     object Home : Screen(route = "home")
     object List : Screen(route = "list")
     object Profile : Screen( route = "profile")
-    object Detail : Screen( route= "detail/{itemId]") {
+    object Detail : Screen( route= "detail/{itemId}") {
         fun createRoute(itemId: Int): String = "detail/$itemId"
     }
 }
